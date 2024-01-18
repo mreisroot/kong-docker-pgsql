c9861bd3003a74d36c6adc1a92414a6decb29196
Miguel Reis <miguel_reis08@protonmail.com>
2024-01-18 06:38:08 -0300

    Update interactive menu script

--- menu.sh
+++ menu.sh
@@ -11,14 +11,12 @@ do
 
   # Option menu
   printf "KONG-DOCKER STACK MENU\n\n"
-  for i in $(seq 1 5);
+  for i in $(seq 1 3);
   do
     case "${i}" in
       1) echo "[ ${i} ] - Deploy Stack";;
       2) echo "[ ${i} ] - Destroy Stack";;
-      3) echo "[ ${i} ] - Export Kong Data";;
-      4) echo "[ ${i} ] - Import Kong Data";;
-      5) echo "[ ${i} ] - Exit";;
+      3) echo "[ ${i} ] - Exit";;
     esac
   done
 
@@ -28,8 +26,6 @@ do
   case "${choice}" in
     1) deploy;;
     2) destroy;;
-    3) export_data;;
-    4) import_data;;
-    5) break;;
+    3) break;;
   esac
 done
